f7e33fdbca805a574bf11861c4c22099f0812001
robertoahc85 <robertoahc85@gmail.com>
2024-11-05 21:37:23 -0600

    Configurar SQLite para Render versión gratuita

--- alpago07/settings.py
+++ alpago07/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']  # Cambia '*' por el dominio de Render cuando esté dispon
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Application definition
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'alpago07.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
